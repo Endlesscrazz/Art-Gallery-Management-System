e94e9d924c0d9402484bc6d9f2f0ec649999a59f
BOBBY <bobbypt05@gmail.com>
2018-09-10 13:46:08 +0530

    SQL-Queries Updated

--- SQL-Queries.sql
+++ SQL-Queries.sql
@@ -1,51 +1,62 @@
+CREATE DATABASE IF NOT EXIST Art_Gallery;
 
-CREATE TABLE Artist( Artist_ID INT NOT NULL AUTO_INCREMENT , 
-					 FirstName VARCHAR(20) NOT NULL , 
-					 MiddeName VARCHAR(20) NOT NULL , 
-					 LastName VARCHAR(20) NOT NULL , 
-					 Address VARCHAR(50) NOT NULL , 
-					 Phone INT NOT NULL ,
-					 PRIMARY KEY (Artist_ID));
+USE Art_Gallery;
 
-CREATE TABLE Exhibition( Ex_ID INT NOT NULL AUTO_INCREMENT ,
-						 Ex_Name VARCHAR(20) NOT NULL , 
-						 Location VARCHAR(20) NOT NULL , 
-						 Ex_Type VARCHAR(20) NOT NULL , 
-						 Painter_ID INT NOT NULL , 
-						 Start_Date DATE NOT NULL , 
-						 End_Date DATE NOT NULL , 
-						 PRIMARY KEY (Ex_ID));
+CREATE table Artist(Artist_ID int AUTO_INCREMENT not null primary key, 
+					FirstName varchar(20) not null,
+					MiddleName varchar(20) not null, 
+					LastName varchar(20) not null, 
+					Address varchar(100) not null, 
+					Phone int not null);
 
-CREATE TABLE Gallery( Gallery_ID INT NOT NULL AUTO_INCREMENT , 
-					  Gallery_Name VARCHAR(20) NOT NULL , 
-					  URL VARCHAR(20) NOT NULL , 
-					  Location VARCHAR(20) NOT NULL , 
-					  PRIMARY KEY (Gallery_ID));
+CREATE table Gallery(Gallery_ID int AUTO_INCREMENT not null primary key, 
+					 Gallery_Name varchar(40) not null,
+					 URL varchar(40) not null, 
+					 Location varchar(100) not null );
 
-CREATE TABLE Customer( Customer_ID INT NOT NULL AUTO_INCREMENT , 
-					   Customer_Name VARCHAR(20) NOT NULL , 
-					   Address VARCHAR(20) NOT NULL , 
-					   Phone INT NOT NULL , 
-					   PRIMARY KEY (Customer_ID));
+CREATE table Exhibition(Ex_ID int AUTO_INCREMENT not null primary key, 
+						Ex_Name varchar(40) not null, 
+						Ex_Location varchar(100) not null, 
+						Ex_type varchar(40) not null, 
+						Start_Date date not null, 
+						End_Date date not null);
 
-CREATE TABLE Painting( Title VARCHAR(20) NOT NULL , 
-					   URL VARCHAR(20) NOT NULL , 
-					   Year INT NOT NULL , 
-					   Type VARCHAR(20) NOT NULL , 
-					   Cost INT NOT NULL , 
-					   Likes INT NOT NULL , 
-					   PRIMARY KEY (Title));
+CREATE table Customer(C_ID int AUTO_INCREMENT not null primary key, 
+					  C_Email varchar(40) not null unique, 
+					  C_Name varchar(40) not null, 
+					  Address varchar(100) not null, 
+					  Phone int not null);
 
-ALTER TABLE Gallery ADD FOREIGN KEY (Artist_ID) REFERENCES Artist(Artist_ID);
+CREATE table Painting(Painting_ID int AUTO_INCREMENT not null primary key, 
+					  Title varchar(40) not null, 
+					  URL varchar(40) not null, 
+					  Year year(4) not null, 
+					  Type varchar(40) not null, 
+					  Cost int not null, 
+					  Likes int not null);
 
-ALTER TABLE Painting ADD FOREIGN KEY (Artist_ID) REFERENCES Artist(Artist_ID);
+CREATE table Display(Ex_ID int not null unique, Painting_ID int not null unique);
 
-ALTER TABLE Painting ADD FOREIGN KEY (Customer_ID) REFERENCES Customer(Customer_ID);
+ALTER table Gallery add column Artist_ID int not null;
 
-CREATE TABLE ExhibitionGalleryR( Gallery_ID INT NOT NULL,
-								 Ex_ID INT NOT NULL, 
-								 FOREIGN KEY (Gallery_ID) REFERENCES Gallery(Gallery_ID), 
-								 FOREIGN KEY (Ex_ID) REFERENCES Exhibition(Ex_ID)
-								 PRIMARY KEY (Gallery_ID,Ex_ID) );
+ALTER table Gallery add foreign key(Artist_ID) references Artist(Artist_ID) on delete cascade;
 
 
+ALTER table Painting add column Artist_ID int not null;
+
+ALTER table Painting add foreign key (Artist_ID) references Artist(Artist_ID) on delete cascade;
+
+
+ALTER table Painting add column Gallery_ID int not null;
+
+ALTER table Painting add foreign key (Gallery_ID) references Gallery(Gallery_ID) on delete cascade;
+
+
+ALTER table Painting add column C_ID int not null;
+
+ALTER table Painting add foreign key (C_ID) references Customer(C_ID) on delete cascade;
+
+
+ALTER table Display add foreign key (Ex_ID) references Exhibition(Ex_ID) on delete cascade;
+
+ALTER table Display add foreign key (Painting_ID) references Painting(Painting_ID) on delete cascade;
